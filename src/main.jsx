59760aaf7d722d99c4048cabc044e17d9bea769e
LopezDev <aragonlopezjuan87@gmail.com>
2022-07-31 10:05:24 +0200

    feat: create gobalStyle to reset css to element

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { GlobalStyled } from './globalStyled'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <GlobalStyled />
   </React.StrictMode>
 )
